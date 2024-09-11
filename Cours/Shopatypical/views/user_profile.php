469e38fdbcbacfe8508525f4fd2862c89d79078c
GitpushKen <kendivers@outlook.fr>
2024-09-11 16:50:42 +0200

    Shopatypical progress

--- Cours/Shopatypical/views/user_profile.php
+++ Cours/Shopatypical/views/user_profile.php
@@ -13,39 +13,35 @@ $utilisateurs = $utilisateur->select();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/style.css">
-    <title>Ajout utilisateur</title>
+    <link rel="stylesheet" href="../styles/user_page.css">
+    <title>Shopatypical</title>
 
 </head>
 
 <body>
-<div class="background">
-        <img id="right" src="../assets/imgs/desktop_bg/right.svg" alt="right">
-        <img id="full-left" src="../assets/imgs/desktop_bg/full_left.svg" alt="full-left">
-        <img id="top-left" src="../assets/imgs/desktop_bg/top_left.svg" alt="top-left">
+    <div class="background">
+            <img id="right" src="../assets/imgs/desktop_bg/right.svg" alt="right">
+            <img id="full-left" src="../assets/imgs/desktop_bg/full_left.svg" alt="full-left">
+            <img id="top-left" src="../assets/imgs/desktop_bg/top_left.svg" alt="top-left">
     </div>
     <header>
-        <a class="logo" href="../index.php">
-        <img src="../assets/imgs/logo.svg" alt="logo">
-        </a>
-        
-
-        <div class="profile">
-        <?php 
-            if(!empty($_SESSION['username'])) {
-                ?> 
-                <a href="#">
-                <img src="../assets/imgs/profile-connected.png" alt="profile-icon">
-                </a>
-                <div class="cheering">
-                <a class="cheer" href="../connexion/deconnexion.php">Se déconnecter</a>
-
-                </div>
-            <?php } ?>
-
-            <!-- <a class="cheer" href="./connexion/deconnexion.php">Se déconnecter</a> -->
-        </div>
+            <a class="logo" href="../index.php">
+            <img src="../assets/imgs/logo.svg" alt="logo">
+            </a>
+            <div class="profile">
+                    <a href="#">
+                    <img src="../assets/imgs/profile-connected.png" alt="profile-icon">
+                    </a>
+                    <div class="cheering">
+                        <a class="cheer" href="../connexion/deconnexion.php">Se déconnecter</a>
+                    </div>
+            </div>
     </header>
+    <main>
+        <div id="profile-grid">
+
+        </div>
+    </main>
 
 </body>
 
